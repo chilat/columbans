9f09cea9571237383258f8e294822273c5b9d741
Emmanuel <emmanuel.rop@gmail.com>
2014-04-25 11:47:33 -0700

    Changing the name

--- admin/login.php
+++ admin/login.php
@@ -25,7 +25,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">SCA-Student Information and Grading System</a>
+          <a class="navbar-brand" href="index.php">Student Information and Grading System</a>
         </div>
 
        
